73a92fdf72abaeee55835bdcfa3b9878f8f9a7e0
Sampurna <sampurng@gmail.com>
2024-01-10 14:29:02 -0500

    FIRST API SUCCESSS YADFBHIAHDFAI DFAKJD

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use std::ptr::null;
+
+use rocket::http::Method;
+use rocket_cors::{AllowedOrigins, AllowedHeaders};
+
 #[macro_use] 
 extern crate rocket;
 mod connection;
@@ -7,8 +12,15 @@ mod collections;
 #[launch]
 fn rocket() -> _ {
     let t = connection::connect_to_mongo();
+    let allowed_origins = AllowedOrigins::some_exact(&["http://localhost:19006/"]);
     
-    // let t: Result<(), mongodb::error::Error> = connect_to_mongo();
+    let cors = rocket_cors::CorsOptions {
+        allowed_origins,
+        allowed_methods: vec![Method::Post].into_iter().map(From::from).collect(),
+        allowed_headers: AllowedHeaders::some(&["Authorization", "Accept", "Content-type", "Access-Control-Allow-Origin"]),
+        allow_credentials: true,
+        ..Default::default()
+    }.to_cors().unwrap();
     rocket::build()
-        .mount("/api", routes![controllers::signup::index, controllers::signup::connections, controllers::signup::lol])
+        .mount("/api", routes![controllers::signup::index, controllers::signup::connections, controllers::signup::lol]).attach(cors)
 }
